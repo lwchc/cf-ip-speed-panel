83ede622b05f5da16812d3efe5b9d2a11365fa37
10000ge10000 <huipengbotr@gmail.com>
2026-06-06 13:35:28 +0800

    Split LuCI settings and logs into tabs

--- openwrt-packages/luci-app-cf-ip-speed-client/htdocs/luci-static/resources/view/services/cf_ip_speed_client.js
+++ openwrt-packages/luci-app-cf-ip-speed-client/htdocs/luci-static/resources/view/services/cf_ip_speed_client.js
@@ -71,54 +71,56 @@ return view.extend({
 
     var s = m.section(form.NamedSection, 'main', 'client');
     s.anonymous = true;
+    s.tab('basic', _('基本设置'));
+    s.tab('log', _('日志'));
 
-    var links = s.option(form.DummyValue, '_project_links', _('\u9879\u76ee\u94fe\u63a5'));
+    var links = s.taboption('basic', form.DummyValue, '_project_links', _('\u9879\u76ee\u94fe\u63a5'));
     links.rawhtml = true;
     links.cfgvalue = function() {
       return '<a href="https://cf.6610000.xyz/" target="_blank" rel="noopener noreferrer">\u9879\u76ee\u9875\u9762</a>'
         + ' | <a href="https://github.com/10000ge10000/cf-ip-speed-panel" target="_blank" rel="noopener noreferrer">GitHub</a>';
     };
 
-    var o = s.option(form.Flag, 'enabled', _('\u542f\u7528'));
+    var o = s.taboption('basic', form.Flag, 'enabled', _('\u542f\u7528'));
     o.default = '0';
     o.rmempty = false;
 
-    o = s.option(form.Value, 'nickname', _('\u6635\u79f0'));
+    o = s.taboption('basic', form.Value, 'nickname', _('\u6635\u79f0'));
     o.description = _('\u6635\u79f0\u5148\u5230\u5148\u5f97\uff0c\u6ce8\u518c\u540e\u4f1a\u5c55\u793a\u5728\u8d21\u732e\u5217\u8868\u4e2d\u3002');
     o.placeholder = '\u4e00\u4e07AI\u5206\u4eab';
     o.rmempty = false;
 
-    o = s.option(form.ListValue, 'schedule_mode', _('\u6d4b\u901f\u65b9\u5f0f'));
+    o = s.taboption('basic', form.ListValue, 'schedule_mode', _('\u6d4b\u901f\u65b9\u5f0f'));
     o.value('interval', _('\u5468\u671f\u6027\u6d4b\u901f'));
     o.value('daily', _('\u6bcf\u5929\u5b9a\u65f6\u6d4b\u901f'));
     o.default = 'interval';
     o.rmempty = false;
 
-    o = s.option(form.Value, 'interval_hours', _('\u5468\u671f\u6d4b\u901f\u95f4\u9694\uff08\u5c0f\u65f6\uff09'));
+    o = s.taboption('basic', form.Value, 'interval_hours', _('\u5468\u671f\u6d4b\u901f\u95f4\u9694\uff08\u5c0f\u65f6\uff09'));
     o.default = '6';
     o.datatype = 'range(1,168)';
     o.rmempty = false;
     o.depends('schedule_mode', 'interval');
 
-    o = s.option(form.Value, 'daily_hour', _('\u6bcf\u5929\u6d4b\u901f\u65f6\u95f4\uff08\u5c0f\u65f6\uff09'));
+    o = s.taboption('basic', form.Value, 'daily_hour', _('\u6bcf\u5929\u6d4b\u901f\u65f6\u95f4\uff08\u5c0f\u65f6\uff09'));
     o.default = '3';
     o.datatype = 'range(0,23)';
     o.rmempty = false;
     o.depends('schedule_mode', 'daily');
 
-    o = s.option(form.Value, 'daily_minute', _('\u6bcf\u5929\u6d4b\u901f\u65f6\u95f4\uff08\u5206\u949f\uff09'));
+    o = s.taboption('basic', form.Value, 'daily_minute', _('\u6bcf\u5929\u6d4b\u901f\u65f6\u95f4\uff08\u5206\u949f\uff09'));
     o.default = '0';
     o.datatype = 'range(0,59)';
     o.rmempty = false;
     o.depends('schedule_mode', 'daily');
 
-    o = s.option(form.DummyValue, '_daily_hint', _('\u95f2\u65f6\u63d0\u9192'));
+    o = s.taboption('basic', form.DummyValue, '_daily_hint', _('\u95f2\u65f6\u63d0\u9192'));
     o.cfgvalue = function() {
       return _('\u5efa\u8bae\u5c3d\u91cf\u9009\u62e9\u6bcf\u5929\u95f2\u65f6\u6d4b\u901f\uff0c\u4f8b\u5982\u51cc\u6668 3 \u70b9\u81f3 5 \u70b9\uff0c\u907f\u514d\u5f71\u54cd\u6b63\u5e38\u4ee3\u7406\u4e0a\u7f51\u4f53\u9a8c\u3002');
     };
     o.depends('schedule_mode', 'daily');
 
-    o = s.option(form.ListValue, 'log_clear_interval', _('日志定时清理'));
+    o = s.taboption('log', form.ListValue, 'log_clear_interval', _('日志定时清理'));
     o.value('never', _('不自动清理'));
     o.value('daily', _('每天清理'));
     o.value('weekly', _('每周清理'));
@@ -126,19 +128,19 @@ return view.extend({
     o.default = 'weekly';
     o.rmempty = false;
 
-    o = s.option(form.ListValue, 'log_max_size', _('日志大小上限'));
+    o = s.taboption('log', form.ListValue, 'log_max_size', _('日志大小上限'));
     o.value('102400', '100 KB');
     o.value('1048576', '1 MB');
     o.value('5242880', '5 MB');
     o.default = '1048576';
     o.rmempty = false;
 
-    o = s.option(form.DummyValue, 'device_id', _('\u8bbe\u5907 ID'));
+    o = s.taboption('basic', form.DummyValue, 'device_id', _('\u8bbe\u5907 ID'));
     o.cfgvalue = function(section_id) {
       return this.map.data.get('cf_ip_speed_client', section_id, 'device_id') || _('\u672a\u6ce8\u518c');
     };
 
-    o = s.option(form.DummyValue, 'last_status', _('\u6700\u8fd1\u72b6\u6001'));
+    o = s.taboption('basic', form.DummyValue, 'last_status', _('\u6700\u8fd1\u72b6\u6001'));
     o.rawhtml = true;
     o.cfgvalue = function(section_id) {
       var status = this.map.data.get('cf_ip_speed_client', section_id, 'last_status') || '-';
@@ -152,7 +154,7 @@ return view.extend({
       return parts.join('<br />');
     };
 
-    var registerButton = s.option(form.Button, '_register', _('\u6ce8\u518c\u6635\u79f0'));
+    var registerButton = s.taboption('basic', form.Button, '_register', _('\u6ce8\u518c\u6635\u79f0'));
     registerButton.inputstyle = 'apply';
     registerButton.onclick = function() {
       showResult(_('\u6ce8\u518c\u6635\u79f0'), _('\u6b63\u5728\u6ce8\u518c\uff0c\u8bf7\u7a0d\u5019...'), false);
@@ -165,7 +167,7 @@ return view.extend({
       });
     };
 
-    var runButton = s.option(form.Button, '_run', _('\u7acb\u5373\u6d4b\u901f\u5e76\u4e0a\u4f20'));
+    var runButton = s.taboption('basic', form.Button, '_run', _('\u7acb\u5373\u6d4b\u901f\u5e76\u4e0a\u4f20'));
     runButton.inputstyle = 'action';
     runButton.onclick = function() {
       showResult(_('\u6d4b\u901f\u4e0a\u4f20'), _('\u6b63\u5728\u542f\u52a8\u540e\u53f0\u6d4b\u901f\u4efb\u52a1\uff0c\u8bf7\u7a0d\u5019...'), false);
@@ -178,7 +180,7 @@ return view.extend({
       });
     };
 
-    var logButton = s.option(form.Button, '_show_log', _('查看日志'));
+    var logButton = s.taboption('log', form.Button, '_show_log', _('查看日志'));
     logButton.inputstyle = 'action';
     logButton.onclick = function() {
       showResult(_('运行日志'), _('正在读取日志...'), false);
@@ -189,7 +191,7 @@ return view.extend({
       });
     };
 
-    var clearLogButton = s.option(form.Button, '_clear_log', _('清空日志'));
+    var clearLogButton = s.taboption('log', form.Button, '_clear_log', _('清空日志'));
     clearLogButton.inputstyle = 'remove';
     clearLogButton.onclick = function() {
       showResult(_('清空日志'), _('正在清空日志...'), false);
